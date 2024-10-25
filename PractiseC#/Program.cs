d47655122db8bc14d203fc91bd040e6247c1bcb9
Khizar Akhtar <110335678+Khizar-Akhtar@users.noreply.github.com>
2024-10-25 21:40:50 +0000

    Pending changes exported from your codespace

---
+++ PractiseC#/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel;
+
+List<string> movies = new List<string>(3);
+List <int> number=new List<int>(4);
+
+
+
+number.AddRange(new int[] { 1, 2, 3, 4 });
+
+
+foreach (int num in number)
+{
+    Console.WriteLine(num);
+}
+
